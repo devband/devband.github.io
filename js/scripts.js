2eab817ab6d9fdc1bf2c4b65a23ecdd5fa5ee730
Alexey Sholohov <asolohov@gmail.com>
2016-09-06 10:38:00 +0300

    start developing swipe page

--- js/scripts.js
+++ js/scripts.js
@@ -53,12 +53,76 @@ function hidden_menu(target) {
 }
 
 /*
- *  Copy number purse
+ *		Copy number purse
  */
 
 function copyPurseNumber(link) {
 	prompt("Вы можете скопировать номер кошелька:", link.title);
 }
+
+/*
+ *		Swipe
+ */
+
+// Переход по страницам 
+
+var pages = ['index.html','projects.html','contacts.html'];
+location.href.indexOf( 'index.html' );
+
+function changePageSwipe() {
+	pages.forEach(function(item,i,arr) {
+		
+	});
+}
+
+// -----
+
+var startPoint = {};
+var nowPoint;
+var ldelay;
+document.addEventListener('touchstart', function(event) {
+//	event.preventDefault();
+	event.stopPropagation();
+	startPoint.x = event.changedTouches[0].pageX;
+	startPoint.y = event.changedTouches[0].pageY;
+	ldelay = new Date();
+}, false);
+/*Ловим движение пальцем*/
+document.addEventListener('touchmove', function(event) {
+	event.preventDefault();
+	event.stopPropagation();
+	var otk = {};
+	nowPoint = event.changedTouches[0];
+	otk.x = nowPoint.pageX - startPoint.x;
+	/*Обработайте данные*/
+	/*Для примера*/
+	if (Math.abs(otk.x) > 200) {
+		if (otk.x < 0) { /*СВАЙП ВЛЕВО(ПРЕД.СТРАНИЦА)*/
+			
+		}
+		if (otk.x > 0) { /*СВАЙП ВПРАВО(СЛЕД.СТРАНИЦА)*/
+			
+		}
+		startPoint = {
+			x: nowPoint.pageX,
+			y: nowPoint.pageY
+		};
+	}
+}, false);
+/*Ловим отпускание пальца*/
+document.addEventListener('touchend', function(event) {
+	var pdelay = new Date();
+	nowPoint = event.changedTouches[0];
+	var xAbs = Math.abs(startPoint.x - nowPoint.pageX);
+	var yAbs = Math.abs(startPoint.y - nowPoint.pageY);
+	if ((xAbs > 20 || yAbs > 20) && (pdelay.getTime() - ldelay.getTime()) < 200) {
+		if (xAbs > yAbs) {
+			if (nowPoint.pageX < startPoint.x) { /*СВАЙП ВЛЕВО*/ } else { /*СВАЙП ВПРАВО*/ }
+		} else {
+			if (nowPoint.pageY < startPoint.y) { /*СВАЙП ВВЕРХ*/ } else { /*СВАЙП ВНИЗ*/ }
+		}
+	}
+}, false);
 
 /*
  *		Gallery	
